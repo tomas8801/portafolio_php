63766a98625ff7bdf69c33156277131c473f7c5a
Tomas Marsili <tomasmarsili.contacto@hotmail.com>
2020-05-21 20:31:22 -0300

    Changes in the slide

---
+++ views/contact.php
@@ -0,0 +1,36 @@
+<div class="col-lg-12 item contacto d-none">
+
+    <div class="card">
+
+        <div class="card-head " id="card-form">
+            <p class="text-center mt-3">PHP Full Stack Developer student at Educacion IT, Buenos Aires.</p>
+            <p class="text-center"><a href="https://www.facebook.com/tommi.marsili">Facebook</a> - <a href="https://www.linkedin.com/in/tomasmarsili/">Linkedin</a> - <a href="https://github.com/tomas8801">Github</a><br>Or</p>
+
+            <form style="width: 500px;" class="form mx-auto" id="formulario" action="enviar.php" method="post">
+
+                <div class="form-group ">
+                    <input class="form-control " placeholder="Enter your name" type="text" name="nombre" id="nombre" required/>
+                </div>
+
+                <div class="form-group">
+                    <input class="form-control" placeholder="Enter your email" type="email" name="email" id="email" required/>
+                </div>
+
+                <div class="form-group">
+                    <textarea  class="form-control" placeholder="Enter your comment" name="mensaje" id="mensaje" cols="30" rows="5" required></textarea>
+                </div>
+
+                <div class="form-group">
+                    <input class="btn form-control" type="submit" id="enviar" value="Send mail" name="submit"/>
+                </div>
+
+            </form>
+        </div>
+
+        <div class="card-body text-center">
+            <h4 class="title">Tomas Marsili</h4>
+        </div>
+
+    </div>
+
+</div>
